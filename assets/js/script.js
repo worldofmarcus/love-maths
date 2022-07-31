93e89ce14bf668e9567cabc7d3942ff586904543
worldofmarcus <me@marcuseriksson.com>
2022-07-31 15:01:21 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,10 +11,11 @@ document.addEventListener("DOMContentLoaded", function() {
             alert("You clicked Submit!");
         }else {
             let gameType = this.getAttribute("data-type");
-            alert(`You clicked ${gameType}`);
-        }
+            runGame(gameType);
+                }
     })
     }
+    runGame("addition");
 })
 
 
@@ -22,11 +23,19 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the user's answer has been processed
  */
 
-function runGame(){
+function runGame(gameType){
 
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random()*25 + 1);
     let num2 = Math.floor(Math.random()*25 + 1);
+
+    if (gameType === "addition"){
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
+    
 }
 
 function checkAnswer(){
@@ -46,8 +55,10 @@ function incrementWrongAnswer(){
 
 }
 
-function displayAdditionQuestion(){
-
+function displayAdditionQuestion(operand1, operand2){
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 }
 
 function displaySubtractQuestion(){
